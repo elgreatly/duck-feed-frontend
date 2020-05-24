c01a855697748e9621fa8d1841307f20d3697ce1
elgreatly <abdo.elgreatly@gmail.com>
2020-05-24 20:47:14 +0200

    fix build issues

--- src/app/modules/duck-feed-form/duck-feed-form.module.ts
+++ src/app/modules/duck-feed-form/duck-feed-form.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DuckFeedFormComponent } from './component/duck-feed-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgbTimepicker} from '@ng-bootstrap/ng-bootstrap';
+import { NgbTimepickerModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 
 @NgModule({
-  declarations: [DuckFeedFormComponent, NgbTimepicker],
+  declarations: [DuckFeedFormComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbTimepickerModule
   ],
   exports: [DuckFeedFormComponent]
 })
